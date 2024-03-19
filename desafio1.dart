346b7ff00a15a04f36b8a00daa9f50f14566bfc3
NandaDuarte <82245037+NandaDuarte@users.noreply.github.com>
2024-03-19 15:47:16 -0300

    implementado função desafio1

--- desafio1.dart
+++ desafio1.dart
@@ -1,3 +1,12 @@
 void main() {
-  // Escreva o código aqui...
+  int valorA = 92;
+  int valorB = 92;
+
+  if(valorA > valorB) {
+    print("o valor A é maior que o valor B");
+  } else if(valorB > valorA) {
+    print("o valor B é maior que o valor A");
+  } else {
+    print("o valor A é igual ao valor B");
+  }
 }
