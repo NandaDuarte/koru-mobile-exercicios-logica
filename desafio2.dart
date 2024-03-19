1f3b8f24e2eb0913ac3bb7ea616ae2cbeca7571f
NandaDuarte <82245037+NandaDuarte@users.noreply.github.com>
2024-03-19 16:27:25 -0300

    implemantado função desafio2

--- desafio2.dart
+++ desafio2.dart
@@ -1,3 +1,17 @@
+/**Crie uma função que inicialize os valores de A, B, C. Como resultado, imprima a soma entre A e B 
+  e se a soma é maior ou menor do que C.*/
 void main() {
-  // Escreva o código aqui...
+  int valorA = 2;
+  int valorB = 30;
+  int valorC = 70;
+  int soma = valorA + valorB; 
+  
+if(soma > valorC ){
+  print("a soma $soma > valorC $valorC");
+
+}
+else{
+  print("soma $soma é menor que $valorC");
+}
+  
 }
