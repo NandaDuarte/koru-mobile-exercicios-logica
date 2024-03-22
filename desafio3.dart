22ee58c769dec34d8337dd2bf07b40433cc10258
NandaDuarte <82245037+NandaDuarte@users.noreply.github.com>
2024-03-22 14:33:05 -0300

    implementando função desafio3

--- desafio3.dart
+++ desafio3.dart
@@ -1,3 +1,27 @@
+/*Crie uma função que inicialize um número qualquer. Como resultado, imprima se o número é par ou ímpar e positivo ou negativo.
+*/
+
+void desafio3(){
+  int numero  = 9;
+ int resto = 9 % 2;
+ if(resto == 0){
+print( "par");
+ }
+ 
+else {
+  print("impar");
+}
+
+if (resto >= 0){
+  print("o numero é positivo");
+  }
+
+  else{
+    print("o numero é negativo");
+  }
+}
+
 void main() {
-  // Escreva o código aqui...
+desafio3(); 
+
 }
